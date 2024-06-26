6b42320330825162fc65836df204e2c3a65aa4b4
Daniel Augusto Moore da Silva <dagmoores@gmail.com>
2024-06-25 22:36:12 -0300

    add

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,49 @@
+import Modelos.Compras;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        int status = 1;
+        Compras compras = new Compras();
+
+        System.out.println("Digite o limite do cartao:");
+        int limite = scanner.nextInt();
+
+        while (status != 0) {
+            if (status == 1) {
+
+                // Consumir a nova linha pendente
+                scanner.nextLine();
+
+                System.out.println("Digite a descricao do item");
+                String descricao = scanner.nextLine();
+
+                System.out.println("Digite o valor da compra");
+                int valor = scanner.nextInt();
+                    if (limite - valor < 0) {
+                        System.out.println("\nSua compra excedeu o limite de seu cartão.");
+                        compras.informacao(limite);
+                    }
+                    else {
+                        limite-= valor;
+                        compras.listaUpdate(descricao, valor, limite);
+                    }
+
+
+                System.out.println("Para continuar Digite 1, para cancelar digite 0");
+                status = scanner.nextInt();
+            } else {
+                System.out.println("Comando desconhecido");
+                System.out.println("Para continuar Digite 1, para cancelar digite 0");
+                status = scanner.nextInt();
+            }
+            if (status == 0) {
+                compras.informacao(limite);
+            }
         }
     }
 }
--- src/Modelos/Compras.java
+++ src/Modelos/Compras.java
@@ -1,2 +1,42 @@
-package Modelos;public class Compras {
+package Modelos;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Compras {
+    protected String descricao;
+    protected int valor;
+    protected List<HashMap<String, Integer>> lista = new ArrayList();
+
+
+
+    public void listaUpdate(String descricao, int valor, int limite) {
+        HashMap<String, Integer> map = new HashMap<>();
+        this.descricao = descricao;
+        this.valor = valor;
+
+        System.out.println("Compra realizada!");
+        System.out.println("Seu novo limite é: " + limite);
+        map.put(descricao, valor);
+        lista.add(map);
+
+    }
+
+    public void informacao(int limite) {
+                System.out.println(
+                "************************************ \n" +
+                        "COMPRAS REALIZADAS: \n");
+
+        for (HashMap<String, Integer> item : lista) {
+            for (String chave : item.keySet()) {
+                System.out.println(chave + " - " + item.get(chave));
+            };
+        }
+
+        System.out.println("************************************ \n" +
+                "Saldo do Cartão: " + limite  + "\n"
+        );
+
+    }
 }
